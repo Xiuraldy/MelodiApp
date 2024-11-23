994804ded790c366e08275d825117e9a09d879b7
Xiuraldy <xiuraldy.9@gmail.com>
2024-11-23 13:49:11 -0500

    change cors

--- api/shared/middlewares.go
+++ api/shared/middlewares.go
@@ -14,11 +14,10 @@ import (
 
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Origin", "http://54.207.125.158")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Header("Access-Control-Allow-Private-Network", "true")
-		c.Header("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
